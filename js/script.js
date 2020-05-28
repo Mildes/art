6c987d6fee81ba2d7d203cb04bfd8a9d69b4214b
Valentina <64679930+Mildes@users.noreply.github.com>
2020-05-28 03:27:01 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -44,21 +44,5 @@ $(document).ready(function () {
 	button.addEventListener('click', function () {
 		swal("Спасибо за заявку!", "Я отвечу Вам в ближайшее время.", "success");
 	});
-	server {
-    listen       80;
-    server_name  localhost;
-
-    location / {
-        root   html;
-        index  index.html index.htm;
-    }
-
-    error_page  404     /404.html;
-    error_page  403     /403.html;
-
-    error_page  405     =200 $uri;
-
-    # ...
-}
 
 });
